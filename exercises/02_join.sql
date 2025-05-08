80e9c5763f8017ee268de18d6bbc43a8e2b3ad3e
minhi0449 <minhi0449@gmail.com>
2025-05-08 22:37:46 +0900

    docs(join): 정리용 JOIN 주석 추가 - INNER/LEFT/RIGHT/FULL/CROSS/SELF 개념 정리

---
+++ exercises/02_join.sql
@@ -0,0 +1,9 @@
+
+/*
+	INNER JOIN:   [A ∩ B] - A와 B 모두 존재하는 행
+	LEFT JOIN:    [A + (A ∩ B)] - A의 모든 행 + 조건에 맞는 B
+	RIGHT JOIN:   [B + (A ∩ B)] - B의 모든 행 + 조건에 맞는 A
+	FULL JOIN:    [A ∪ B] - A와 B의 모든 행
+	CROSS JOIN:   [A × B] - A와 B의 모든 조합
+	SELF JOIN:    [A = A] - 동일 테이블을 자신과 조인
+*/
