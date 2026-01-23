661482fee2a8874582fa5b44e1a8ce04581f889f
minhi0449 <minhi0449@gmail.com>
2026-01-23 23:15:46 +0900

    경기통계+경기데이터드림

---
+++ sqld/경기통계+경기데이터드림.sql
@@ -0,0 +1,90 @@
+SELECT
+    A.DTFILE_ID AS "dtfileId",
+    F.DTTRAN_ID AS "dttranId",
+    E.UPL_SCH_NO AS "uplSchNo",
+    D.DT_NM AS "dtNm",
+    D.INF_NM AS "infNm",
+    A.SHT_NM AS "shtNm",
+    D.LOAD_CD AS "loadCd",
+    
+    (
+        SELECT ORG_NM
+        FROM TB_COMM_ORG
+        WHERE ORG_CD = E.ORG_CD
+            AND USE_YN = 'Y'
+    ) AS "orgNm",
+    
+    (
+        SELECT USR_NM
+        FROM TB_COMM_USR
+        WHERE USR_ID = F.REG_ID
+    ) AS "usrNm",
+    
+    (
+        SELECT USR_CD
+        FROM TB_COMM_USR
+        WHERE USR_ID = F.REG_ID
+    ) AS "usrCd",
+    
+    (
+        SELECT USR_TEL
+        FROM TB_COMM_USR
+        WHERE USR_ID = F.REG_ID
+    ) AS "usrTel",
+    
+    (
+        SELECT USR_EMAIL
+        FROM TB_COMM_USR
+        WHERE USR_ID = F.REG_ID
+    ) AS "usrEmail",
+    TO_CHAR(F.END_DTTM, 'YYYY-MM-DD HH24:MI:SS') AS "chkDttm",
+    TO_CHAR(COALESCE(
+    TO_CHAR(ROUND((F.END_DTTM - F.STRT_DTTM) * 24 * 60 * 60)), 
+    '0'
+)) AS "chkTm",
+    COALESCE(F.CHCK_YN, 'N') AS "chckYn",
+    CASE 
+        WHEN F.CHCK_YN = 'N' THEN 
+            CASE 
+                WHEN F.ERR_CNT = 0 THEN F.PROC_RSLT 
+                ELSE F.ERR_CNT::text || ' 건의 오류가 발생하였습니다.' 
+            END
+        ELSE F.PROC_RSLT 
+    END AS "procRslt",
+    CASE 
+        WHEN F.PROC_STAT = 'T' THEN '예'
+        WHEN F.PROC_STAT = 'I' THEN '예(이어서저장하기)'
+        WHEN F.PROC_STAT = 'P' THEN '예'
+        WHEN F.PROC_STAT = 'U' THEN '예'
+        WHEN F.PROC_STAT = 'D' THEN '예'
+        WHEN F.PROC_STAT = 'M' THEN '예'
+        ELSE '아니오'
+    END AS "saveYn",
+    F.DATA_COND_DTTM AS "dataCondDttm",
+    F.LOAD_YMD AS "loadYmd"
+FROM TB_OPEN_DTFILE A
+INNER JOIN (
+    SELECT
+        B.INF_ID,
+        B.INF_NM,
+        C.DT_NM,
+        B.LOAD_CD,
+        B.MULTI_MNG_YN
+    FROM TB_OPEN_INF B
+    INNER JOIN TB_OPEN_DT C ON B.DT_ID = C.DT_ID
+) D ON A.INF_ID = D.INF_ID AND A.DEL_YN = 'N'
+INNER JOIN (
+    SELECT
+        LOAD_YMD,
+        DTFILE_ID,
+        UPL_SCH_NO,
+        ORG_CD
+    FROM TB_UPLOAD_SCHE
+    WHERE DEL_YN = 'N'
+) E ON A.DTFILE_ID = E.DTFILE_ID
+INNER JOIN TB_OPEN_DTTRAN F 
+    ON E.UPL_SCH_NO = F.UPL_SCH_NO
+    AND F.DEL_YN = 'N'
+    AND F.PROC_STAT IN ('I', 'C', 'E', 'P', 'U', 'D', 'M', 'T', 'I')
+WHERE 1 = 1
+ORDER BY F.DTTRAN_ID DESC
